86594e5690d8c9030781c5136488f99bcb7abcee
konatsup <konatsukomatsu@gmail.com>
2018-12-19 01:24:34 +0900

    Create Count.vue

---
+++ src/components/Count.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="counter">Count</div>
+</template>
+
+<script>
+export default {
+  name: "Count"
+};
+</script>
+
+<style scoped>
+.counter {
+  font-size: 26px;
+  background-color: #dfd;
+}
+</style>
